ad7411e40a3728aa759012eb42e42f54c41cc4e9
Amber Swevers <amber.swevers@hotmail.com>
2025-05-07 20:29:10 +0200

    init

---
+++ src/app/layout.jsx
@@ -0,0 +1,33 @@
+import { Providers } from '@/app/providers'
+import { Layout } from '@/components/Layout'
+
+import '@/styles/tailwind.css'
+
+export const metadata = {
+  title: {
+    template: '%s - Amber Swevers',
+    default:
+      'Amber Swevers - Frontend developer and UI/UX designer',
+  },
+  description:
+    'Hi! I’m Amber, a frontend developer and UI/UX designer based in Vlaams-Brabant, Belgium. I have a background as a consultant and been part of varying projects, each with their own approach and technologies. ',
+  alternates: {
+    types: {
+      'application/rss+xml': `${process.env.NEXT_PUBLIC_SITE_URL}/feed.xml`,
+    },
+  },
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" className="h-full antialiased" suppressHydrationWarning>
+      <body className="flex h-full bg-zinc-50 dark:bg-black">
+        <Providers>
+          <div className="flex w-full">
+            <Layout>{children}</Layout>
+          </div>
+        </Providers>
+      </body>
+    </html>
+  )
+}
