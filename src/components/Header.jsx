2a6822f7d0ec1a39f350ebf65efbf275c0312cb9
Amber Swevers <amber.swevers@hotmail.com>
2025-05-11 10:20:13 +0200

    coloring theme button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -182,8 +182,8 @@ function ThemeToggle() {
       className="group rounded-full bg-white/90 px-3 py-2 shadow-lg ring-1 shadow-zinc-800/5 ring-zinc-900/5 backdrop-blur-sm transition dark:bg-zinc-800/90 dark:ring-white/10 dark:hover:ring-white/20"
       onClick={() => setTheme(otherTheme)}
     >
-      <SunIcon className="h-6 w-6 fill-zinc-100 stroke-zinc-500 transition group-hover:fill-zinc-200 group-hover:stroke-zinc-700 dark:hidden [@media(prefers-color-scheme:dark)]:fill-teal-50 [@media(prefers-color-scheme:dark)]:stroke-teal-500 [@media(prefers-color-scheme:dark)]:group-hover:fill-teal-50 [@media(prefers-color-scheme:dark)]:group-hover:stroke-teal-600" />
-      <MoonIcon className="hidden h-6 w-6 fill-zinc-700 stroke-zinc-500 transition dark:block [@media_not_(prefers-color-scheme:dark)]:fill-teal-400/10 [@media_not_(prefers-color-scheme:dark)]:stroke-teal-500 [@media(prefers-color-scheme:dark)]:group-hover:stroke-zinc-400" />
+      <SunIcon className="h-6 w-6 fill-zinc-100 stroke-zinc-500 transition group-hover:fill-orange-200 group-hover:stroke-zinc-700 dark:hidden [@media(prefers-color-scheme:dark)]:fill-lime-600 [@media(prefers-color-scheme:dark)]:stroke-lime-600 [@media(prefers-color-scheme:dark)]:group-hover:fill-lime-600 [@media(prefers-color-scheme:dark)]:group-hover:stroke-lime-600" />
+      <MoonIcon className="hidden h-6 w-6 fill-zinc-700 stroke-zinc-500 transition dark:block [@media_not_(prefers-color-scheme:dark)]:fill-lime-600/10 [@media_not_(prefers-color-scheme:dark)]:stroke-lime-600 [@media_not_(prefers-color-scheme:dark)]:group-hover:stroke-orange-300" />
     </button>
   )
 }
